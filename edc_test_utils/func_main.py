73e10d5f40269917b20b166979f9d0b4e66611b6
erikvw <ew2789@gmail.com>
2022-09-14 21:16:35 -0500

    change signature for func_main

--- edc_test_utils/func_main.py
+++ edc_test_utils/func_main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import sys
 
 import django
@@ -5,11 +7,11 @@ from django.conf import settings
 from django.test.runner import DiscoverRunner
 
 
-def func_main(app_name, project_settings):
+def func_main(project_settings, *project_tests):
     if not settings.configured:
         settings.configure(**project_settings)
     django.setup()
     tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
     failfast = any([True for t in sys.argv if t.startswith("--failfast")])
-    failures = DiscoverRunner(failfast=failfast, tags=tags).run_tests([f"{app_name}.tests"])
+    failures = DiscoverRunner(failfast=failfast, tags=tags).run_tests(project_tests)
     sys.exit(failures)
