d64f8d874cccce31200aae6813647da1047255a7
erikvw <ew2789@gmail.com>
2023-03-23 12:35:01 -0500

    fix database settings

--- edc_test_utils/default_test_settings.py
+++ edc_test_utils/default_test_settings.py
@@ -276,7 +276,7 @@ class DefaultTestSettings:
                 "ENGINE": "django.db.backends.mysql",
                 "NAME": "test",
                 "USER": "root",
-                "PASSWORD": "cc3721b",
+                "PASSWORD": "password",
                 "HOST": "127.0.0.1",
                 "PORT": 3306,
             }
@@ -288,7 +288,7 @@ class DefaultTestSettings:
                         "ENGINE": "django.db.backends.mysql",
                         "NAME": "other",
                         "USER": "root",
-                        "PASSWORD": "cc3721b",
+                        "PASSWORD": "password",
                         "HOST": "127.0.0.1",
                         "PORT": 3306,
                     }
