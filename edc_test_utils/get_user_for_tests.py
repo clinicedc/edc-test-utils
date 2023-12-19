1e4acec4bd5ae8fd13fe1cc2db866a5b2ab65ad4
erikvw <ew2789@gmail.com>
2023-12-18 22:21:13 -0600

    get_user_for_tests

--- edc_test_utils/get_user_for_tests.py
+++ edc_test_utils/get_user_for_tests.py
@@ -2,16 +2,32 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
+from edc_sites.site import SiteNotRegistered, sites
 from edc_sites.utils import get_site_model_cls
 
 if TYPE_CHECKING:
     from django.contrib.auth.models import User
 
 
-def get_user_for_tests(username: str | None = None) -> User:
+def get_user_for_tests(
+    username: str | None = None,
+    codenames: list[tuple[str, str]] | None = None,
+    view_only: bool | None = None,
+) -> User:
+    """Returns a User model instance ready for tests.
+
+    Adds:
+        - user permissions to something (default is Appointment)
+        - a site to the user's userprofile
+
+    `edc_sites` and `edc_auth` are a required apps
+    for INSTALLED_APPS in runtests.
+
+    Tests against edc dashboard like Views need the
+    user to have permissions.
+    """
     user = get_user_model().objects.create_superuser(
         username or "user_login", "u@example.com", "pass"
     )
@@ -19,10 +35,29 @@ def get_user_for_tests(username: str | None = None) -> User:
     user.is_staff = True
     user.save()
     user.refresh_from_db()
-    user.userprofile.sites.add(get_site_model_cls().objects.get(id=settings.SITE_ID))
-    user.user_permissions.add(
-        Permission.objects.get(
-            codename="view_appointment", content_type__app_label="edc_appointment"
+
+    # at least on site should be registered. You can
+    #  - use settings.EDC_SITES_REGISTER_DEFAULT=True
+    #  - manually register in the test or test setup.
+    #  - austodiscover sites.py in your app or test_app
+    if not sites.all():
+        raise SiteNotRegistered(
+            "Failed to create test user. Cannot update userprofile.sites. "
+            "No sites have been registered!"
         )
-    )
+    # will raise an exception if edc_auth not installed
+    user.userprofile.sites.add(get_site_model_cls().objects.get_current())
+
+    if codenames is None:
+        codenames = [
+            ("edc_appointment", "view_appointment"),
+            ("edc_appointment", "add_appointment"),
+            ("edc_appointment", "change_appointment"),
+        ]
+    for app_label, codename in codenames:
+        if view_only and not codename.startswith("view_"):
+            continue
+        user.user_permissions.add(
+            Permission.objects.get(codename=codename, content_type__app_label=app_label)
+        )
     return user
