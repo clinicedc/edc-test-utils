9d1774285dcc06df46ada7cc0a4d45b73b16260b
erikvw <ew2789@gmail.com>
2024-04-17 08:10:54 -0500

    remove unused

--- edc_test_utils/__init__.py
+++ edc_test_utils/__init__.py
@@ -1,3 +1,2 @@
-from .default_test_settings import DefaultTestSettings  # noqa
-from .func_main import func_main  # noqa
-from .show_urls import show_url_names, show_urls  # noqa
+from .default_test_settings import DefaultTestSettings
+from .func_main import func_main
